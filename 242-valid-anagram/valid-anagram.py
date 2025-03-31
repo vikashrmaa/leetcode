4302930537bd01cab016cc15a232303ac7495f9d
Vikash Sharma <168404374+vikashrmaa@users.noreply.github.com>
2025-03-31 19:51:46 +0530

    Time: 15 ms (43.37%) | Memory: 17.8 MB (64.46%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -4,12 +4,11 @@ class Solution:
             return False
         countT = {}
         countS = {}
-        
+
         for i in range(len(s)):
-
-            countS[s[i]] = 1 + countS.get(s[i], 0)
             countT[t[i]] = 1 + countT.get(t[i], 0)
-
-        return True if countT == countS else False
-            
-            
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+        if countT == countS:
+            return True
+        else:
+            return False 

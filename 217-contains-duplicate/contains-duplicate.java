a5c6ce6b12e097c92be5054154845111ef68b53f
Vikash Sharma <168404374+vikashrmaa@users.noreply.github.com>
2025-04-13 17:04:35 +0530

    Time: 12 ms (49.59%) | Memory: 58.4 MB (37.09%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,18 +1,12 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        // Create a HashSet to track seen elements
         Set<Integer> seen = new HashSet<>();
-        // Iterate through the array
-        for (int num : nums) {
-            // If the element is already in the set, a duplicate exists
-            if (seen.contains(num))
+        for (int num : nums){
+            if(seen.contains(num))
                 return true;
-            // Otherwise, add the element to the set
             else
                 seen.add(num);
         }
-        // No duplicates found
         return false;
     }
-}   
-    
+}

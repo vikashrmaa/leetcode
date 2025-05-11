e75697b4c37fb3d12adf8b2fffeddd1ca5c4657b
Vikash Sharma <168404374+vikashrmaa@users.noreply.github.com>
2025-05-12 01:05:15 +0530

    Time: 5 ms (70.85%) | Memory: 17.9 MB (39.19%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -2,7 +2,6 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         newstr = str(x)
         if newstr == newstr[::-1]:
-            return True 
+            return True
         else:
             return False
-            

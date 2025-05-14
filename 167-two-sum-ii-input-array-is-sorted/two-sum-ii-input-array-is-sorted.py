198132aa4fd3af72a3593da7339d8251215e732f
Vikash Sharma <168404374+vikashrmaa@users.noreply.github.com>
2025-05-14 17:26:10 +0530

    Time: 7 ms (25.34%) | Memory: 18.7 MB (33.59%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,10 +1,10 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        n = len(numbers)
-        l = 0
-        r = n - 1
-        while l < r:
-            if numbers[l] + numbers[r] < target:
+        n = len(numbers) #lem of number in n
+        l = 0   # L will be at 0th position in array
+        r = n - 1  #to put r at the last position of an array
+        while l < r: #loop will work untill l < r
+            if numbers[l] + numbers[r] < target: 
                 l = l + 1
             elif numbers[l] + numbers[r] > target:
                 r = r - 1

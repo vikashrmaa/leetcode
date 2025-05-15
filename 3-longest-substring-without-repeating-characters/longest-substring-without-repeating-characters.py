b184048aaa0f3a30cf8020f3b33cc071ab9e890e
Vikash Sharma <168404374+vikashrmaa@users.noreply.github.com>
2025-05-15 16:40:29 +0530

    Time: 20 ms (40.50%) | Memory: 17.7 MB (82.26%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -3,12 +3,11 @@ class Solution:
         check = set()
         ans = 0
         l = 0
-
         for r in range(len(s)):
             while s[r] in check:
                 check.remove(s[l])
                 l += 1
             check.add(s[r])
-            ans = max(ans , r-l+1)
-        return ans                
+            ans = max(ans, r-l+1)
+        return ans       
         

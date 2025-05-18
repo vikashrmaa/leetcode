d83c90eef20d35b9d51742b1a14f1dc4c90a6a48
Vikash Sharma <168404374+vikashrmaa@users.noreply.github.com>
2025-05-19 01:26:49 +0530

    Time: 47 ms (76.66%) | Memory: 34.4 MB (32.06%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -4,8 +4,7 @@ class Solution:
         longest = 0
 
         for num in numset:
-            # Only start when it's the beginning of a sequence
-            if num - 1 not in numset:
+           if num - 1 not in numset:
                 length = 1
                 while num + length in numset:
                     length += 1
